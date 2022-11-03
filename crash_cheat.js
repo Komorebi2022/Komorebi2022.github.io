814e30ddd915ba43152376dd8f759ca3db7950eb
Komorebi2022 <y1366066294@163.com>
2022-11-03 18:33:54 +0800

    Site updated: 2022-11-03 18:33:54

---
+++ crash_cheat.js
@@ -0,0 +1,12 @@
+
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/TEP.ico");
+         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/favicon.ico");
+         document.title = '(ฅ>ω
